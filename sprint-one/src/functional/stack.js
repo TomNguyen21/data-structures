1372987a6459eafed6316ea0a34cc5a471311051
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-07-01 16:14:07 -0700

    Finish Functional Stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,31 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (storage[0] === undefined) {
+      storage[0] = value;
+    } else {
+      var storageLength = someInstance.size();
+      for (var i = 0; i <= storageLength; i++) {
+        if (storage[i] === undefined) {
+          storage[i] = value;
+        }
+      }
+    }
+    return someInstance.size;
   };
 
   someInstance.pop = function() {
+    var storageLength = someInstance.size();
+    var removedElement = storage[storageLength - 1]
+    delete storage[storageLength - 1];
+    console.log(storage)
+    return removedElement;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    // console.log(keys.length)
+    return keys.length;
   };
 
   return someInstance;
