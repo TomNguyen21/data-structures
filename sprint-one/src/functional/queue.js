f79fa833324ff4f63d8d4fbda13b0a464422cea2
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-01 13:48:31 -0700

    completed queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,82 @@
-var Queue = function() {
-  var someInstance = {};
+var Queue = function () {
+  var someInstance = {
+    count: 0
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+  };
 
   // Implement the methods below
-
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    if (!someInstance.first) {
+      someInstance.count++;
+      storage[someInstance.count] = value;
+    } else {
+      someInstance.count++;
+      storage[someInstance.count] = value;
+    }
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    if (!someInstance.count) {
+      return
+    }
+    let deletedValue = storage['1'];
+    delete storage['1'];
+    someInstance.count--;
+
+    //create deleted value variable
+    //delete
+    //decrement count in someInstance
+    //currentLength
+    //let currentIndex = 1;
+    let currentIndex = 1;
+    //while loop thhrough the keys until lengths and
+    for (let oldNums in storage) {
+      storage[currentIndex] = storage[oldNums];
+      currentIndex++;
+    }
+    return deletedValue
   };
 
-  someInstance.size = function() {
+
+  someInstance.size = function () {
+    return someInstance.count
   };
 
   return someInstance;
+
 };
+
+// var Stack = function () {
+//   var someInstance = {
+//     count: 0
+//   };
+
+//   // Use an object with numeric keys to store values
+//   var storage = {
+//   };
+
+//   // Implement the methods below
+//   someInstance.push = function (value) {
+//     someInstance.count++;
+//     storage[someInstance.count] = value;
+//   };
+
+//   someInstance.pop = function () {
+//     if (!someInstance.count) {
+//       return
+//     }
+//     let deletedItem = storage[someInstance.count]
+//     delete storage[someInstance.count];
+//     someInstance.count--;
+//     return deletedItem
+//   };
+
+//   someInstance.size = function () {
+//     return someInstance.count;
+//   };
+
+//   return someInstance;
+// };
