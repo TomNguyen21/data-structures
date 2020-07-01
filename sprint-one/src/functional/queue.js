f0082501b455380896ce4c4ec358644e964bfdb6
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-01 14:02:20 -0700

    cleaned up functional queue and stack js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -25,14 +25,7 @@ var Queue = function () {
     let deletedValue = storage['1'];
     delete storage['1'];
     someInstance.count--;
-
-    //create deleted value variable
-    //delete
-    //decrement count in someInstance
-    //currentLength
-    //let currentIndex = 1;
     let currentIndex = 1;
-    //while loop thhrough the keys until lengths and
     for (let oldNums in storage) {
       storage[currentIndex] = storage[oldNums];
       currentIndex++;
@@ -48,35 +41,3 @@ var Queue = function () {
   return someInstance;
 
 };
-
-// var Stack = function () {
-//   var someInstance = {
-//     count: 0
-//   };
-
-//   // Use an object with numeric keys to store values
-//   var storage = {
-//   };
-
-//   // Implement the methods below
-//   someInstance.push = function (value) {
-//     someInstance.count++;
-//     storage[someInstance.count] = value;
-//   };
-
-//   someInstance.pop = function () {
-//     if (!someInstance.count) {
-//       return
-//     }
-//     let deletedItem = storage[someInstance.count]
-//     delete storage[someInstance.count];
-//     someInstance.count--;
-//     return deletedItem
-//   };
-
-//   someInstance.size = function () {
-//     return someInstance.count;
-//   };
-
-//   return someInstance;
-// };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
-var Stack = function() {
-  var someInstance = {};
+var Stack = function () {
+  var someInstance = {
+    count: 0
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+  };
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    someInstance.count++;
+    storage[someInstance.count] = value;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    if (!someInstance.count) {
+      return
+    }
+    let deletedValue = storage[someInstance.count];
+    delete storage[someInstance.count];
+    someInstance.count--;
+    return deletedValue
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return someInstance.count;
   };
 
   return someInstance;
