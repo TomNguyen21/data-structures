e56512236136f22d4fc459472fff6343aed0d3e1
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-01 19:10:27 -0700

    completed protypal stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
-var Stack = function() {
+//use object create
+
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  let initiate = Object.create(stackMethods);
   // but try not not reference your old code in writing the new style.
+  return initiate
 };
 
-var stackMethods = {};
+var stackMethods = {
+  count: 0,
+  push: function (value) {
+    this.count++;
+    this[this.count] = value
+  },
+  pop: function () {
+    if (!this.count) {
+      return
+    }
+    let deletedValue = this[this.count];
+    delete this[this.count];
+    this.count--;
+    return deletedValue
+  },
+  size: function () {
+    return this.count
+  }
+};
 
 
