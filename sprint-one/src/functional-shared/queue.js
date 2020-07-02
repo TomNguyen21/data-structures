7cf5aea9b06ec35b6f8f30b13ba202f38fe62d8d
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-01 17:57:37 -0700

    finished all of queue but the final test. Will return to it

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = {
+    count: 0
+  };
+  _.extend(instance, queueMethods);
+  return instance
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function (value) {
+    this.count++;
+    this[this.count] = value;
+  },
+  dequeue: function () {
+    if (!this.count) {
+      return
+    } else {
+      let deletedValue = this['1'];
+      this.count--;
+      delete this['1'];
+      let index = 1;
+      for (let oldNums in this) {
+        if (Number(oldNums) === 'number') {
+          this[index] = this[oldNums];
+        }
+      }
+      return deletedValue
+    }
+  },
+  size: function () {
+    return this.count
+  }
+};
