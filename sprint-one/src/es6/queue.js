4a71f3a269fdf52aa06461a98df69711050da61d
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-07-01 23:50:19 -0700

    Finish ES6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+    return this.counter;
+  };
+  dequeue() {
+    if (this.counter === 0) {
+      return undefined;
+    } else {
+      var removedElement = this.storage[0];
+      for (var i = 0; i <= this.counter; i++) {
+        if (this.storage[i + 1] !== undefined) {
+          this.storage[i] = this.storage[i + 1];
+        }
+      }
+      this.counter--;
+      return removedElement;
+    }
+  }
+  size() {
+    return this.counter;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
+
+  someInstance = {};
+
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+    return this.counter;
+  };
+  pop() {
+    if (this.counter === 0) {
+      return undefined;
+    } else {
+      this.counter--;
+      return this.storage[this.counter];
+    }
+  };
+  size() {
+    return this.counter;
+  };
 
 }
