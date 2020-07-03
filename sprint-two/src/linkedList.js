701e5dffc17a1ce35f5df4222e37f81d7e502546
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-02 18:34:52 -0700

    completed linked list and trees

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,6 +2,7 @@ var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
+
   list.addToTail = function (value) {
     let newNode = new Node(value);
     if (this.head === null) {
@@ -11,10 +12,14 @@ var LinkedList = function () {
       let currentNode = this.head;
       while (currentNode.next !== null) {
         currentNode = currentNode.next;
+
       }
-      currentNode.next = newNode;
+      currentNode.next = newNode
       this.tail = newNode;
+      //alternate way of doing it?
+      // list.tail.next = newNode;
     }
+
   };
 
   list.removeHead = function () {
@@ -25,8 +30,6 @@ var LinkedList = function () {
 
   list.contains = function (target) {
     let currentNode = this.head;
-    console.log(currentNode)
-    console.log(currentNode.value)
     while (currentNode.next !== null) {
       if (currentNode.value === target) {
         return true
@@ -38,16 +41,13 @@ var LinkedList = function () {
     }
     return false
   };
-
   return list;
 };
 
 var Node = function (value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
@@ -56,15 +56,18 @@ call Node to create a new object {}
 add to original list object
 if this.head is null newNode is set and tail
 else set only tail
-
-
-
 */
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail
+ O(n) because its loopede through all nodes until the tail
+removeHead. Our method is O(n) but can also be O(1). We will come back
+O(1) deletes the head instantly because we have access to it
+contains
+O(n) It loops until the end to find the target
  */
 
  //does this work
