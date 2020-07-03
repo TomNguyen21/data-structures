b296101c283b35252d3c2d33dcd3f2b40f901a18
Jorge Garcia <jorgarcia1994@gmail.com>
2020-07-02 21:47:44 -0700

    completed sets file

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  //this.storage.push(item)
+  this.storage.push(item)
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  // let storageArray = this.storage
+  let storageArray = this.storage;
+  // for let i = 0; i < storageArray.length; i++
+  for (let i = 0; i < storageArray.length; i++) {
+    if (storageArray[i] = item) {
+      return true;
+    }
+  }
+  return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  let deletedItemIndex = this.storage.indexOf(item);
+  this.storage.splice(deletedItemIndex, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add - O(1) - always pushing to the end of the array
+contains - O(n) Iterate to find the target value.
+remove  -O(n) Iterate once to find the index and another time to replace/reassign values
  */
